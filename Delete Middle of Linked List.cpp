df8e0acaf0a101f01ba869b62bd19eb808e10bea
Aayush Anand <121629475+aayush751@users.noreply.github.com>
2024-04-28 17:08:57 +0530

    Create Delete Middle of Linked List.cpp

---
+++ Delete Middle of Linked List.cpp
@@ -0,0 +1,21 @@
+class Solution{
+    public:
+    Node* deleteMid(Node* head)
+    {
+        // Your Code Here
+        
+        if( head -> next == NULL )
+        {
+            return NULL;
+        }
+        Node* fast=head, *slow=head, *prev=NULL;
+        while(fast && fast->next){
+            fast=fast->next->next;
+            prev=slow;
+            slow=slow->next;
+        }
+        
+        prev->next=slow->next;
+        return head;
+    }
+};
