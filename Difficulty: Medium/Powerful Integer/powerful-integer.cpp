b300ca86b76d3a9dd3f5cdf5c3ebdcde01f8f914
Aayush Anand <121629475+aayush751@users.noreply.github.com>
2025-07-31 22:07:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Powerful Integer/powerful-integer.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int powerfulInteger(vector<vector<int>>& intervals, int k) {
+        // code here
+         map<int,int> mm;
+        for(auto x:intervals){
+            mm[x[0]]++;
+            mm[x[1]+1]--;
+        }
+        int temp=0,ans=-1;
+        for(auto x:mm){
+            if(temp>=k and x.second<0)ans=x.first-1;
+            temp+=x.second;
+            if(temp>=k)ans=x.first;
+        }
+        return ans;
+    }
+};
