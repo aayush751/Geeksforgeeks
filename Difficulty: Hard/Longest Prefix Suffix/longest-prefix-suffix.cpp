daad418e48f7d1ba8c005a30fd0afe6b50b605d5
Aayush Anand <121629475+aayush751@users.noreply.github.com>
2025-08-08 16:36:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Longest Prefix Suffix/longest-prefix-suffix.cpp
+++ Difficulty: Hard/Longest Prefix Suffix/longest-prefix-suffix.cpp
@@ -1,70 +1,22 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-// User function template for C++
-
 class Solution {
   public:
-    int lps(string str) {
-        // Your code goes here
-        int n = str.size();
-            
-        vector<int> lps(n, 0);
-        
-        int length = 0;
-        int i = 1;
-        
-        while ( i < n ) 
-        {
-            if ( str[i] == str[length] ) 
-            {
-                length++;
-                lps[i] = length;
-                i++;
-            } 
-            else 
-            {
-                if (length != 0) 
-                {
-                    length = lps[length - 1];
-                } 
-                else 
-                {
-                    lps[i] = 0;
-                    i++;
-                }
+    int getLPSLength(string &str) {
+        // code here
+         int n=str.length();
+        int p=0,s=1,pos=1,count=0;
+        while(p<n && s<n){
+            if(str[p]==str[s]){
+                p++;
+                s++;
+                count++;
+            }
+            else{
+                p=0;
+                pos++;
+                s=pos;
+                count=0;
             }
         }
-        return lps[n - 1];
+        return count;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-
-    ios_base::sync_with_stdio(0);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    int t;
-    cin >> t;
-    while (t--) {
-        string str;
-        cin >> str;
-
-        Solution ob;
-
-        cout << ob.lps(str) << "\n";
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
