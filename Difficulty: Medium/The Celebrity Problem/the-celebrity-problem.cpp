84916544811487e4efa5990f3ec8e0445d70bf70
Aayush Anand <121629475+aayush751@users.noreply.github.com>
2025-08-30 10:50:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
+++ Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
@@ -1,74 +1,20 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function template for C++
-
 class Solution {
   public:
-    // Function to find if there is a celebrity in the party or not.
-    int celebrity(vector<vector<int> >& mat) {
+    int celebrity(vector<vector<int>>& mat) {
         // code here
-        int ans = -1;
-        int n = mat.size();
-        
-        for(int i=0; i<n; i++ )
-        {
-            bool rowZero = true;
-            
-            for(int j=0; j<n; j++)
-            {
-                if( mat[i][j] == 1 ) 
-                {
-                    rowZero = false;
-                    break;
-                }
-            }
-            
-            if( rowZero == true )
-            {
-                bool colOne = true;
-                
-                for( int j=0; j<n; j++ )
-                {
-                    if(j == i ) continue;
-                    
-                    if( mat[j][i] == 0 )
-                    {
-                        colOne = false;
-                        break;
-                    }
-                }
-                
-                if( rowZero && colOne ) 
-                    return i;
-            }
+         int n=mat.size();
+        int a=0;
+        int b=n-1;
+        while(a<b){
+            if(mat[a][b])
+            a++;
+            else
+            b--;
         }
-        return -1;
+        for(int i=0;i<n;i++){
+            if((i!=a) && (mat[a][i] || !mat[i][a]))
+            return -1;
+        }
+        return a;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        vector<vector<int> > M(n, vector<int>(n, 0));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                cin >> M[i][j];
-            }
-        }
-        Solution ob;
-        cout << ob.celebrity(M) << endl;
-    }
-}
-
-// } Driver Code Ends
