f1f31ee8853450faa0186e3c796b519a124ab415
Aayush Anand <121629475+aayush751@users.noreply.github.com>
2025-09-12 11:38:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimize the Heights II/minimize-the-heights-ii.cpp
+++ Difficulty: Medium/Minimize the Heights II/minimize-the-heights-ii.cpp
@@ -1,11 +1,3 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 // User function template for C++
 
 class Solution {
@@ -39,28 +31,3 @@ class Solution {
         return ans;
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        int n, k;
-        cin >> k;
-        cin.ignore();
-        vector<int> a, b, c, d;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int num;
-        while (ss >> num)
-            a.push_back(num);
-
-        Solution ob;
-        auto ans = ob.getMinDiff(a, k);
-        cout << ans << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
