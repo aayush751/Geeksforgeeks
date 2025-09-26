ab4d6d8ff9f629ff4a21b1370e1289f8d95d122f
Aayush Anand <121629475+aayush751@users.noreply.github.com>
2025-09-26 21:15:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Rotate Deque By K/rotate-deque-by-k.java
@@ -0,0 +1,40 @@
+class Solution {
+    public static void rotateDeque(Deque<Integer> dq, int type, int k) {
+        // code here
+        int n = dq.size();
+        k=k%n;
+        
+        //store the ele in arr
+        int arr[] = new int[n];
+        int size=n,i=0;
+        while(size!=0){
+            arr[i++] = dq.removeFirst();
+            size--;
+        }
+        
+        //
+        if(type==1){// left rotate by k places
+            revArray(arr, 0, n-1 );
+            revArray(arr, 0, k-1 );
+            revArray(arr, k, n-1 );
+        }  
+        else{
+             revArray(arr, 0, k-1 );
+            revArray(arr, k, n-1 );
+            revArray(arr, 0, n-1 );
+        }
+        
+        for(int x : arr){
+            dq.add(x);
+        }
+    }
+    static void revArray( int arr[] , int l, int h){
+        while(l<h){
+            int temp = arr[l];
+            arr[l] = arr[h];
+            arr[h] = temp;
+            
+            l++; h--;
+        }
+    }
+}
