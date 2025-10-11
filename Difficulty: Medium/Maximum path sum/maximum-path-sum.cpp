7fb4f452809006aadbe4b96ad151ddade5e4b98a
Aayush Anand <121629475+aayush751@users.noreply.github.com>
2025-10-11 11:42:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Maximum path sum/maximum-path-sum.cpp
@@ -0,0 +1,54 @@
+// User Fuction template for C++
+/*
+// Tree Node
+class Node {
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+    // Function to return maximum path sum from any node in a tree.
+    
+    int solve( Node* root, int &maxi )
+    {
+        if( root == nullptr )
+        {
+            return 0;
+        }
+        
+        int leftSum = solve( root -> left, maxi );
+        int rightSum = solve( root -> right, maxi );
+        
+        if( leftSum > 0 and rightSum > 0 )
+        {
+            maxi = max( maxi, root -> data + leftSum + rightSum );
+        }
+        else if( leftSum < 0 and rightSum < 0  )
+        {
+            maxi = max( maxi, root -> data );
+        }
+        else
+        {
+            maxi = max( maxi, root -> data + max( leftSum, rightSum ) ); 
+        }
+        
+        return max(0, root -> data + max( leftSum, rightSum ) ) ;
+    }
+    
+    int findMaxSum(Node *root) {
+        // code here
+        int maxi = INT_MIN;
+        
+        solve( root, maxi );
+        
+        return maxi;
+    }
+};
