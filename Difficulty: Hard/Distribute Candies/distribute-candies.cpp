7f5dc33b8f19ec2994ef111e42cb6e516d88b374
Aayush Anand <121629475+aayush751@users.noreply.github.com>
2026-03-18 16:08:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Distribute Candies/distribute-candies.cpp
@@ -0,0 +1,62 @@
+/*
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int x) {
+        data = x;
+        left = right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    int distCandy(Node* root) {
+        // code here
+        map< Node *, int> subtree, candies;
+        int ans = 0;
+        
+        function< void(Node *)> dfs1 = [&](Node * node )
+        {
+            if( !node )
+            {
+                return;
+            }
+            
+            subtree[node] = 1;
+            candies[node] = node -> data;
+            
+            dfs1( node -> left );
+            dfs1( node -> right );
+            
+            subtree[node] += subtree[node -> left ] + subtree[node -> right ];
+            candies[node] += candies[node -> left ] + candies[node -> right ];
+        };
+        
+        dfs1( root );
+        
+        function< void( Node *) > dfs2 = [&]( Node * node )
+        {
+            if( !node )
+            {
+                return;
+            }
+            
+            int leftextra = candies[node -> left ] - subtree[ node -> left ];
+            
+            int rightextra = candies[node -> right ] - subtree[ node -> right ];
+            
+            ans += abs( leftextra) + abs(rightextra);
+            
+            dfs2( node -> left );
+            dfs2( node -> right );
+        };
+        
+        dfs2( root );
+        
+        return ans;
+    }
+};
